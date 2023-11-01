c6e7ba616728486f6b4dbc0260f5e22aa12a8c2f
André Ferreira <110822583+andre9277@users.noreply.github.com>
2023-11-01 21:13:08 +0000

    Added styling for Store navbar section
    Added static data to populate data for the store navbar option;
    Style added with button to add to cart, a remove button and a "+" and "-" button;
    Added one utility to convert the price to the right currency.

--- EcommerceProject/src/pages/Store.tsx
+++ EcommerceProject/src/pages/Store.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
 
 const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <h1>Store</h1>;
+      {/* for medium, small and large screen sizes, with gap of 3 */}
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            {" "}
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Store
+export default Store;
