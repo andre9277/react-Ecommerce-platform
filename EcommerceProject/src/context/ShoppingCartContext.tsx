d29838a0459749cbc8e5be07ddef4cbccd40dbc4
André Ferreira <110822583+andre9277@users.noreply.github.com>
2023-11-03 11:20:08 +0000

    Added sidebar for shoppingcart
    With bootsrap i developed a side menu for the shopping cart

--- EcommerceProject/src/context/ShoppingCartContext.tsx
+++ EcommerceProject/src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
@@ -22,6 +23,7 @@ type CartItem = {
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
+//custom hook
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
@@ -102,6 +104,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
